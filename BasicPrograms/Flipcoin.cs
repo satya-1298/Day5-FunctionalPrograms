e8aac5cc55ed79f49c8d12f7e47324a2d3a6d271
unknown <satya-1298>
2023-06-19 13:42:41 +0530

    [Satya]Added-Fliping coin head or tail

---
+++ BasicPrograms/Flipcoin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class Flipcoin
+    {
+        public void Display()
+        {
+            Random r = new Random();
+            int flip = r.Next(3);
+            Console.WriteLine(flip);    
+            if (flip == 0) 
+            {
+                Console.WriteLine("It's Head");
+            }
+            else
+            {
+                Console.WriteLine("It's Tail");
+            }
+
+        }
+    }
+}
---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Programs");
+
+            Console.WriteLine("Choose a option 1-flipcoin ");
+            int choice=Convert.ToInt32(Console.ReadLine());
+            switch(choice) 
+            {
+                case 1:
+                    Console.WriteLine("The Flip coin Program");
+                    Flipcoin f=new Flipcoin();
+                    f.Display();
+                    break;
+                default:
+                    Console.WriteLine("Entered Wrong choice");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
