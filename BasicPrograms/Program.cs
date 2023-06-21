a5bf0ccc81077071eb2a7c9c818651bb4b584764
unknown <satya-1298>
2023-06-21 11:52:20 +0530

    [Satya]Added-Compute Reminder and Quotient

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -13,41 +13,46 @@ namespace BasicPrograms
             Console.WriteLine("Welcome to Basic Programs");
             int n;
             Console.WriteLine("Choose a option 1-flipcoin, 2-LeadYear ,3-PowerOfTwo " +
-                "4-Harmonic Number ,5-FactorofNumber");
-            int choice=Convert.ToInt32(Console.ReadLine());
+                "4-Harmonic Number ,5-FactorofNumber, 6-Division");
+            int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
-                {
-                    case 1:
-                        Console.WriteLine("The Flip coin Program");
-                        Flipcoin f = new Flipcoin();
-                        f.Display();
-                        break;
-                    case 2:
-                        Console.WriteLine("Lead Year");
-                        LeapYear le = new LeapYear();
-                        le.Year();
-                        break;
-                     case 3:
-                        Console.WriteLine("Power of two Program");
-                        Power power = new Power();
-                        power.Oftwo();
-                        break;
-                     case 4:
-                        Console.WriteLine("Harmonic Series Program");
-                        Harmonic h = new Harmonic();
-                        h.Series();
-                        break;
+            {
+                case 1:
+                    Console.WriteLine("The Flip coin Program");
+                    Flipcoin f = new Flipcoin();
+                    f.Display();
+                    break;
+                case 2:
+                    Console.WriteLine("Lead Year");
+                    LeapYear le = new LeapYear();
+                    le.Year();
+                    break;
+                case 3:
+                    Console.WriteLine("Power of two Program");
+                    Power power = new Power();
+                    power.Oftwo();
+                    break;
+                case 4:
+                    Console.WriteLine("Harmonic Series Program");
+                    Harmonic h = new Harmonic();
+                    h.Series();
+                    break;
                 case 5:
                     Console.WriteLine("FactorsofNumber Program");
                     Factors factors = new Factors();
                     factors.Ofnumber();
                     break;
+                case 6:
+                    Console.WriteLine("Division Program");
+                    Division division = new Division();
+                    division.Math(33, 2);
+                    break;
                 default:
-                        Console.WriteLine("Entered Wrong choice");
-                        break;
-                }
-               
-            
+                    Console.WriteLine("Entered Wrong choice");
+                    break;
+            }
+
+
             Console.ReadLine();
         }
     }
