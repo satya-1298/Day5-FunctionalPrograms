65deea8a76dac5c55ba84f87371d4b197e439b2f
unknown <satya-1298>
2023-06-21 12:01:44 +0530

    [Satya]Added-Swaping two number program

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -13,7 +13,7 @@ namespace BasicPrograms
             Console.WriteLine("Welcome to Basic Programs");
             int n;
             Console.WriteLine("Choose a option 1-flipcoin, 2-LeadYear ,3-PowerOfTwo " +
-                "4-Harmonic Number ,5-FactorofNumber, 6-Division");
+                "4-Harmonic Number ,5-FactorofNumber, 6-Division , 7-SwapingNumbers");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -46,6 +46,11 @@ namespace BasicPrograms
                     Console.WriteLine("Division Program");
                     Division division = new Division();
                     division.Math(33, 2);
+                    break;
+                case 7:
+                    Console.WriteLine("Swaping two numbers");
+                    Swap swap = new Swap();
+                    swap.TwoNumbers(7, 2);
                     break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
